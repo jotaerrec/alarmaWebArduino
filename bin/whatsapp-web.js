ac4cd8b143441419cb4898e9b3e4ae2483710e29
jotaerre <javi.r49915@gmail.com>
2021-10-27 16:13:31 -0300

    📖 DOC: Add Coments

--- bin/whatsapp-web.js
+++ bin/whatsapp-web.js
@@ -1,7 +1,6 @@
 const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const fs = require("fs");
-
 const client = new Client();
 client.on("qr", (qr) => {
   // Generate and scan this code with your phone
@@ -11,4 +10,5 @@ client.on("ready", () => {
   console.log("Client is ready!");
 });
 client.initialize();
+
 module.exports = client;
